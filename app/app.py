e5a3b6e24a35d6e8ce1c7525fbcd37b29ed92d9a
2500031478 <2500031478@kluniversity.in>
2026-07-31 15:28:08 +0530

    initial project is created

--- app/app.py
+++ app/app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
 
+from src.data.load_data import get_summary, load_data
+
 app=Flask(__name__)
 @app.route("/")
 def name():
     return render_template("home.html")
+@app.route("/dataset")
+def dataset():
+
+    df = load_data()
+    summary = get_summary(df)
+    return render_template(
+        "load_dataset.html",
+        summary=summary,
+        first_rows=df.head().to_html(index=False)
+    )
 if __name__=="__main__":
     app.run(debug=True)
